64fcaa35586a91b25d8440faf9d10d7d2b634fe5
moumou <hielf1984@gmail.com>
2021-09-14 23:49:41 +0800

    Update sms_job.rb

--- app/jobs/sms_job.rb
+++ app/jobs/sms_job.rb
@@ -8,24 +8,24 @@ class SmsJob < ApplicationJob
     @version = args[1]
     @message = args[2]
 
-    last_sm = Sm.where("created_at > ?", 15.minutes.ago).last
+    last_sm = Sm.where("created_at > ?", 3.minutes.ago).last
 
     @var = {}
     @var["backtrader_version"] = @version
     @var["message"] = @message
     @status = nil
 
-    # if last_sm.nil?
-    run_time = Time.zone.now
-    current_time = run_time.strftime('%H:%M')
-    uri             = URI.parse("https://api.mysubmail.com/message/xsend.json")
-    sms_appid       = ENV['sms_appid']
-    sms_signature   = ENV['sms_signature']
-    sms_project     = ENV['sms_project']
-    res             = Net::HTTP.post_form(uri, appid: sms_appid, to: cell, project: sms_project, signature: sms_signature, vars: @var.to_json)
+    if last_sm.nil?
+      run_time = Time.zone.now
+      current_time = run_time.strftime('%H:%M')
+      uri             = URI.parse("https://api.mysubmail.com/message/xsend.json")
+      sms_appid       = ENV['sms_appid']
+      sms_signature   = ENV['sms_signature']
+      sms_project     = ENV['sms_project']
+      res             = Net::HTTP.post_form(uri, appid: sms_appid, to: cell, project: sms_project, signature: sms_signature, vars: @var.to_json)
 
-    @status      = JSON.parse(res.body)["status"]
-    # end
+      @status      = JSON.parse(res.body)["status"]
+    end
   end
 # SmsJob.perform_later ENV["admin_phone"], ENV["superme_user"] + " " + ENV["version"], "无法连接"
 
