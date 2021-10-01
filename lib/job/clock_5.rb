bc7ed2277fac0e311ffe91f346a3ff2669c00027
moumou <hielf1984@gmail.com>
2021-10-01 22:23:50 +0800

    clock 5

---
+++ lib/job/clock_5.rb
@@ -0,0 +1,40 @@
+require 'clockwork'
+# require 'clockwork/database_events'
+require '../../config/boot'
+require '../../config/environment'
+require 'active_support/time' # Allow numeric durations (eg: 1.minutes)
+
+module Clockwork
+  # configure do |config|
+  #   config[:sleep_timeout] = 5
+  #   config[:logger] = Logger.new(log_file_path)
+  #   config[:tz] = 'EST'
+  #   config[:max_threads] = 15
+  #   config[:thread] = true
+  # end
+
+  # handler receives the time when job is prepared to run in the 2nd argument
+  handler do |job, time|
+
+    if job == 'huobi.orders_logger'
+      # OrderLoggersJob.perform_later(@symbol, @data)
+      closed_symbols = Rails.cache.redis.hgetall("orders:closing")
+      closed_symbols.each do |cs|
+        symbol = cs[0]
+        data = eval cs[1]
+        ApplicationController.helpers.huobi_orders_log(symbol, data)
+      end
+    end
+
+  end
+
+  every(1.minute, 'huobi.orders_logger')
+  # every(5.minutes, 'huobi.alive_check2', :skip_first_run => true, :thread => true)
+  #
+  # every(1.day, 'midnight.job', :at => '00:00')
+end
+
+# cd /var/www/panda_coins/current/lib/job && clockworkd -c clock.rb start --log -d /var/www/panda_coins/current/lib/job
+# clockworkd -c clock.rb start --log -d /Users/hielf/workspace/projects/panda_coins/lib/job
+# clockworkd -c clock.rb start --log -d /var/www/panda_coins/current/lib/job
+# clockworkd -c clock.rb stop
