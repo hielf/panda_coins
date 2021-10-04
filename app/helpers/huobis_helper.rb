d59e698f73375e831c4716cd1da3b1a29912ad24
moumou <hielf1984@gmail.com>
2021-10-04 22:28:10 +0800

    sidekiq

--- app/helpers/huobis_helper.rb
+++ app/helpers/huobis_helper.rb
@@ -704,4 +704,23 @@ module HuobisHelper
   #   end
   # end
 
+  def sidekiq_queue_clear
+    require 'sidekiq/api'
+    # Clear retry set
+    Sidekiq::RetrySet.new.clear
+    # Clear scheduled jobs
+    Sidekiq::ScheduledSet.new.clear
+    # Clear 'Dead' jobs statistics
+    Sidekiq::DeadSet.new.clear
+    # Clear 'Processed' and 'Failed' jobs statistics
+    Sidekiq::Stats.new.reset
+    # Clear specific queue
+    stats = Sidekiq::Stats.new
+    stats.queues
+    # => {"main_queue"=>25, "my_custom_queue"=>1}
+    queue = Sidekiq::Queue.new('low')
+    queue.count
+    queue.clear
+  end
+
 end
