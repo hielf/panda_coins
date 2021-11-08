6a07ea80fa5d59a0ebd5d26a4cc06e7991cbd04a
moumou <hielf1984@gmail.com>
2021-11-08 22:29:41 +0800

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,9 +10,9 @@
 server '139.162.165.15', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 server '172.105.251.116', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 server '172.104.203.150', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
-server '104.237.132.229', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
-server '172.105.105.229', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
-server '172.104.122.209', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
+server '172.104.203.46', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
+server '172.104.203.27', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
+server '172.104.203.31', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 server '172.104.203.201', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 server '172.104.203.138', port: 122, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
 # server '129.226.126.93', port: 222, roles: [:web, :app, :db], user: "deploy", ssh_options: {user: "deploy"}, primary: true
