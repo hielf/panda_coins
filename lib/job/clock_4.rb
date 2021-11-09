f51ec1fb745df90b39d7ee2af4aa628f2051713b
moumou <hielf1984@gmail.com>
2021-11-09 15:31:42 +0800

    Update clock_4.rb

--- lib/job/clock_4.rb
+++ lib/job/clock_4.rb
@@ -37,8 +37,8 @@ module Clockwork
                 Rails.cache.write("enqueued:closing:#{symbol}", (Time.now.to_f * 1000).to_i, expires_in: 300.second)
                 amount, shares_amount = ApplicationController.helpers.huobi_close_amount(symbol, 2)
                 # OrdersJob.perform_now symbol, 'sell-market', 0, amount, false
-                # OrdersJob.perform_now symbol, 'sell-market', 0, shares_amount, false
-                # OrdersJob.perform_now symbol, 'sell-market', 0, shares_amount, false if amount > shares_amount
+                OrdersJob.perform_now symbol, 'sell-market', 0, shares_amount, false
+                OrdersJob.perform_now symbol, 'sell-market', 0, shares_amount, false if amount > shares_amount
               end
             end
           rescue Exception => e
