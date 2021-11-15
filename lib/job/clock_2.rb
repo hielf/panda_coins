b063a449df996c9fec4380c25bb1558986ed32e7
moumou <hielf1984@gmail.com>
2021-11-15 21:09:00 +0800

    Update clock_2.rb

--- lib/job/clock_2.rb
+++ lib/job/clock_2.rb
@@ -15,73 +15,18 @@ module Clockwork
 
   # handler receives the time when job is prepared to run in the 2nd argument
   handler do |job, time|
-    if job == 'huobi.tickers_check'
-      # check_time = Time.now - 60
-      # tickers = []
-      # tickers = Rails.cache.redis.hgetall("tickers")
-      # if tickers && !tickers.empty?
-      #   c = tickers.find {|x| (eval x[1])[:time] >= check_time}
-      #   return if c && c.count > 0
-      # end
-      current_time = Time.now
-      runtime = Rails.cache.read('running:clock_2')
-      settings = TraderSetting.current_settings
-      if runtime && (current_time - runtime).abs < 30
-        nil
-      else
-        loop do
-          begin
-            start_time = Time.now - settings.tickers_check_interval.to_i
-            end_time = Time.now
 
-            if settings.daily_start_time && !settings.daily_start_time.empty? && end_time <= settings.daily_start_time.to_time
-              # p end_time.strftime("%H:%M:%S")
-              next
-            end
-
-            if end_time >= Time.now.beginning_of_day && end_time <= Time.now.beginning_of_day + settings.tickers_check_interval.to_i
-              start_time = nil
-            end
-
-            symbols = ApplicationController.helpers.huobi_tickers_check(start_time, end_time)
-            open_count, open_symbols = ApplicationController.helpers.huobi_open_symbols(symbols)
-
-            if open_count > 0
-              Rails.logger.warn "openning #{open_count} of new symbols at #{end_time.to_s}"
-              open_symbols.each do |data|
-                symbol = data[0]
-                hash = eval data[1]
-                current_balance = 0
-                begin
-                  rbalance =  Rails.cache.redis.hget("balances", "usdt:trade")
-                  current_balance = (eval rbalance)[:balance].to_f if rbalance
-                  current_trades = Rails.cache.redis.hgetall("trades")
-                  # divide_shares = current_trades.count == 0 ? settings.first_share_divide.to_i : settings.divide_shares.to_i
-                  divide_shares = settings.divide_shares.to_i
-                  amount = (current_balance / (divide_shares - current_trades.count)).truncate(0)
-
-                  OrdersJob.perform_now symbol, 'buy-market', 0, amount, false
-                  Rails.logger.warn "symbol #{symbol} open @ #{hash[:close]} amount: #{amount}"
-                rescue FloatDomainError => e
-                  Rails.logger.warn "symbol #{symbol} open skipped: shares all used"
-                ensure
-                  Rails.cache.redis.hset("balance_his", end_time.strftime("%Y-%m-%d"), {:balance => current_balance})
-                end
-              end
-            end
-
-            sleep 0.2
-          rescue Exception => e
-            Rails.logger.warn "tickers_check job error: #{e.message}"
-          ensure
-            Rails.cache.write('running:clock_2', Time.now, expires_in: 1.minute)
-          end
-        end
+    if job == 'huobi.orders_logger'
+      closed_symbols = Rails.cache.redis.hgetall("orders:closing")
+      closed_symbols.each do |cs|
+        symbol = cs[0]
+        OrderLoggersJob.perform_later symbol
       end
     end
+
   end
 
-  every(1.minute, 'huobi.tickers_check')
+  every(1.minute, 'huobi.orders_logger')
 
   # every(1.minute, 'timing', :skip_first_run => true, :thread => true)
   # every(1.hour, 'hourly.job')
