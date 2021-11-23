2b1934a3f197116e0353288fb4ee2e34d03a35cb
moumou <hielf1984@gmail.com>
2021-11-23 23:37:21 +0800

    Create huobi_tests_helper.rb

---
+++ app/helpers/huobi_tests_helper.rb
@@ -0,0 +1,50 @@
+#export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+# require 'net/ping'
+# require 'pycall/import'
+# include PyCall::Import
+# require 'faye/websocket'
+# require 'eventmachine'
+# ApplicationController.helpers.huobi_tickers_cache
+
+module HuobiTestsHelper
+  # ApplicationController.helpers.huobi_tickers_check(settings, Time.now - 120, Time.now)
+  def huobi_tickers_check_test
+    list = ["titan", "epik", "fis", "mta", "stpt", "mta", "iotx", "iota", "cspr", "skm",
+    "nsure", "act", "aac", "ftorn", "stpt", "talk", "lba", "cvc", "icx", "mdx", "lat"]
+    list.each do |sym|
+      symbol = sym + "usdt"
+      start_time = Time.now.beginning_of_day.strftime("%Y-%m-%dT%H:%M:%S")
+      end_time = (Time.now + 900).strftime("%Y-%m-%dT%H:%M:%S")
+      times = []
+      symbols = []
+      white_list_symbols = ApplicationController.helpers.white_list
+      settings = TraderSetting.current_settings
+      url = "http://postgres.ripple-tech.com:3000/huobi_tickers?and=(time.gte.#{start_time},time.lte.#{end_time},symbol.eq.#{symbol})"
+      result = Faraday.get url
+      json = JSON.parse result.as_json["body"]
+      data_s = json[0]
+
+      if data_s.nil? || data_s.empty?
+        p "#{symbol} no data_s!!"
+      end
+
+      if !white_list_symbols.include? symbol
+        p "not in white_lists!!"
+      end
+
+      p "#{symbol} started.."
+      json.each do |data_l|
+        change = (data_s["close"] == 0 ? 0 : (data_l["close"]-data_s["close"])/data_s["close"])
+        if change >= settings.up_floor_limit.to_f && change <= settings.up_up_limit.to_f
+          p "found #{symbol} #{change} @ #{data_l["time"]}"
+          if data_l["vol"]  >= settings.amount_bottom_limit.to_f
+            p "#{symbol} amount_bottom_limit enough"
+            break
+          end
+        end
+      end;0
+      p "#{symbol} ended.."
+    end;0
+  end
+
+end
