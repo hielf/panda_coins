6383bb8533f8bfaaf572b651cc2feb433b6ff100
moumou <hielf1984@gmail.com>
2021-11-30 21:01:52 +0800

    Create reboot.sh

---
+++ reboot.sh
@@ -0,0 +1,17 @@
+# ssh-copy-id root@139.162.165.15
+# ssh-copy-id root@172.105.251.116
+# ssh-copy-id root@172.104.203.150
+# ssh-copy-id root@172.104.203.46
+# ssh-copy-id root@172.104.203.27
+# ssh-copy-id root@172.104.203.31
+# ssh-copy-id root@172.105.117.115
+# ssh-copy-id root@172.104.203.138
+
+ssh -t root@139.162.165.15 "reboot"
+ssh -t root@172.105.251.116 "reboot"
+ssh -t root@172.104.203.150 "reboot"
+ssh -t root@172.104.203.46 "reboot"
+ssh -t root@172.104.203.27 "reboot"
+ssh -t root@172.104.203.31 "reboot"
+ssh -t root@172.105.117.115 "reboot"
+ssh -t root@172.104.203.138 "reboot"
